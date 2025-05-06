e63f8a98d8bd45c9be7a358e769aa4236ae41663
Thamer Alanazi <thamer51513@gmail.com>
2025-05-06 13:32:02 +0300

    Lab4

---
+++ lab4.js
@@ -0,0 +1,20 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtr(num1, num2) {
+    return num1 - num2;
+}
+
+function multipli(num1, num2) {
+    return num1 * num2;
+}
+
+function division(num1, num2) {
+    return num1 / num2;
+}
+
+console.log(add(5,2));
+console.log(subtr(5,2));
+console.log(multipli(5,2));
+console.log(division(5,2));
